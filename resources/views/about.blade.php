4e699d51fcebf5efd10af1546ed1330173dd30c3
eljesam <124920260+eljesam@users.noreply.github.com>
2024-03-12 21:26:13 +0000

    created abot page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>About Us</h1>
+        <p>Welcome to our about page. Here you can learn more about our company and what we do.</p>
+    </div>
+@endsection
