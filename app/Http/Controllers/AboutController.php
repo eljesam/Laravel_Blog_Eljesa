7a16393ed3c08f3b9f8d5213fbd3bfcda6f04ddb
eljesam <124920260+eljesam@users.noreply.github.com>
2024-03-14 11:26:26 +0000

    Controller created

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -1,1 +1,20 @@
-<?php>
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+   
+    public function index()
+    {
+        return view('about');
+    }
+}
