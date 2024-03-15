fae6c4daab07daeba5b550f0da69f31b0120f800
eljesam <124920260+eljesam@users.noreply.github.com>
2024-03-15 12:07:07 +0000

    created contact page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,20 +7,20 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     
-    
-
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    
-    
-    
-    
     public function index()
     {
-        return view('contact');
+        return view('contact.index');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'surname' => 'required',
+            'email' => 'required',
+            'comment' => 'required'
+        ]);
+        Contact::create($request->all());
+        return redirect()->route('contact.index')
+            ->with('success', 'Your message has been sent successfully');
     }
 }
---
+++ app/Models/Contact.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+
+
+
+
+class Contact extends Model
+{
+    use HasFactory;
+    use Sluggable;
+
+    protected $fillable = ['name', 'surname', 'email', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+}
