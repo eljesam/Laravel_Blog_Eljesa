00246298bddc9c2618a379f732eca4c12c7a4a64
eljesam <124920260+eljesam@users.noreply.github.com>
2024-03-15 10:08:17 +0000

    Styling and editing

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,29 +2,33 @@
 
 @section('content')
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="file:///C:/Users/eljes/Desktop/me.jpg" alt="About Me Image" class="img-fluid">
-                </div>
-                <div class="col-md-6">
-                    <h1>About me</h1>
+        <div class="heading">
+                    <h1>Who am I?</h1>
                     <p>
-                        Hi! I'm Ellie and I do nails.
-                        I am a Bachelor's student at DkIT, studying Computing in Software Development.
+                        Hi! I'm Ellie and I do nails!
+</p>
+                </div>
+                <div class = "container">
+                    <section class="about">
+                        <div class="about-image">
+                            <img src = "images/me.jpg" alt="Ellie">
+                        </div>
+                        <div class="about-content">
+                            <h2>About me</h2>
+                            <p>I am a Bachelor's student at DkIT, studying Computing in Software Development.
                         I have been doing nails for 3 years now and I love it.
 
                         I am a qualified nail technician and I have a passion for nails.
                         I am here to teach you all about your nails and how to keep them healthy.
                         Stay tuned for my blog posts and tutorials!
                     </p>
+                        </div>
+                        </section>
+</div>
                     <div class="social-icons">
-                        <a href="https://www.instagram.com/your_instagram_profile" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.instagram.com/ellie_nailzzzz/" target="_blank"><i class="fab fa-instagram"></i></a>
                         <a href="https://www.facebook.com/your_facebook_profile" target="_blank"><i class="fab fa-facebook"></i></a>
-                        <a href="https://www.youtube.com/your_youtube_channel" target="_blank"><i class="fab fa-youtube"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        <a href="https://www.youtube.com/@NailCareerEducation" target="_blank"><i class="fab fa-youtube"></i></a>
+               
     </div>
 @endsection
