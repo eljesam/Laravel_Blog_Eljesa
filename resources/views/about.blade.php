42729497c40c564f55de5a5413025ce0c52db545
eljesam <124920260+eljesam@users.noreply.github.com>
2024-03-15 09:48:53 +0000

    Styling and editing

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>About Us</h1>
-        <p>Welcome to our about page. Here you can learn more about our company and what we do.</p>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="file:///C:/Users/eljes/Desktop/me.jpg" alt="About Me Image" class="img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <h1>About me</h1>
+                    <p>
+                        Hi! I'm Ellie and I do nails.
+                        I am a Bachelor's student at DkIT, studying Computing in Software Development.
+                        I have been doing nails for 3 years now and I love it.
+
+                        I am a qualified nail technician and I have a passion for nails.
+                        I am here to teach you all about your nails and how to keep them healthy.
+                        Stay tuned for my blog posts and tutorials!
+                    </p>
+                    <div class="social-icons">
+                        <a href="https://www.instagram.com/your_instagram_profile" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/your_facebook_profile" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://www.youtube.com/your_youtube_channel" target="_blank"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
